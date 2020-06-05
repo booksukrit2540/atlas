f7a26e19ee18ec970ee9aeb5641921a83c779b69
prasertcbs <bluecotton.k@gmail.com>
2020-06-05 14:19:33 +0700

    add cylinder fn

--- volume.py
+++ volume.py
@@ -1,5 +1,9 @@
 def cube(side):
     return side ** 3
 
+def cylinder(r, h):
+    return 22/7*r*r*h
+
 if __name__ == "__main__":
     print(f'cube(2) = {cube(2)}')
+    print(f'cylinder(1, 10)={cylinder(1, 10)}')
