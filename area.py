aae607a449cee8eed5b71f63329cd10f7c20b950
prasertcbs <bluecotton.k@gmail.com>
2020-06-05 14:08:04 +0700

    add triangle fn

--- area.py
+++ area.py
@@ -1,5 +1,9 @@
 def rectangle(w, h):
     return w*h
 
+def triangle(w, h):
+    return 1/2*w*h
+
 if __name__ == "__main__":
     print(f'rectangle area = {rectangle(5, 10)}')
+    print(f'triangle area =  {triangle(5, 10)}')
