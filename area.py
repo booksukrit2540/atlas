699123f84455e6f7f0ac4cd8fa9d625d5639e73a
prasertcbs <bluecotton.k@gmail.com>
2020-06-05 15:01:54 +0700

    add circle fn

--- area.py
+++ area.py
@@ -1,9 +1,32 @@
+import math
+
 def rectangle(w, h):
+    """calculate rectangle area
+
+    Args:
+        w (float): width
+        h (float): height
+
+    Returns:
+        float: rectangle area
+    """
     return w*h
 
 def triangle(w: float, h: float)->float:
+    """triangle area
+
+    Args:
+        w (float): width
+        h (float): height
+
+    Returns:
+        float: triangle area
+    """
     return .5*w*h
 
+def circle(r: float)->float:
+    return math.pi * r * r
+    
 if __name__ == "__main__":
     print(f'rectangle area = {rectangle(5, 10)}')
     print(f'triangle area =  {triangle(5, 10)}')
